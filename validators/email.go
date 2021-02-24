7e48c9a1416e3d4878ef841fbd210d33a3852a88
fallenleaves <>
2021-02-24 13:15:28 +0800

    add email validator

--- validators/email.go
+++ validators/email.go
@@ -5,8 +5,11 @@ import (
 )
 
 // Email return a email ValidatorFunc
+// see also https://www.w3.org/TR/2016/REC-html51-20161101/sec-forms.html#email-state-typeemail
 func Email() govalidator.ValidatorFunc {
 	return func(rule *govalidator.Rule, value interface{}, target interface{}) error {
-		return nil
+		vf := Regex("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+		err := vf(rule, value, target)
+		return err
 	}
 }
--- validators/email_test.go
+++ validators/email_test.go
@@ -1,9 +1,65 @@
 package validators_test
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/swordlib/govalidator"
+	"github.com/swordlib/govalidator/validators"
 )
 
 func TestEmail(t *testing.T) {
-
+	t.Run("Basic usage", func(t *testing.T) {
+		type testCase struct {
+			Email string
+			Want  error
+		}
+		vf := validators.Email()
+		testCases := []testCase{
+			{
+				Email: "abcd@gmail.com",
+				Want:  nil,
+			},
+			{
+				Email: "abcd@gmail.com",
+				Want:  nil,
+			},
+			{
+				Email: "job_ms88@163.com",
+				Want:  nil,
+			},
+			{
+				Email: "238483@qq.com",
+				Want:  nil,
+			},
+			{
+				Email: "238483@mail.qq.com",
+				Want:  nil,
+			},
+			{
+				Email: "238483",
+				Want:  errors.New("malformed"),
+			},
+		}
+		for _, tc := range testCases {
+			if got := vf(nil, tc.Email, nil); got != tc.Want {
+				if got == nil {
+					t.Errorf("want: %q, but got: nil", tc.Want)
+				} else if tc.Want == nil {
+					t.Errorf("want: nil, but got: %q", got)
+				} else if got.Error() != tc.Want.Error() {
+					t.Errorf("want: %q, but got: %q", tc.Want, got)
+				}
+			}
+		}
+	})
+	t.Run("Custom message", func(t *testing.T) {
+		vf := validators.Email()
+		want := "Invalid email format"
+		if got := vf(&govalidator.Rule{
+			Message: want,
+		}, "abcd", nil); got.Error() != want {
+			t.Errorf("want: %q, but got: %q", want, got)
+		}
+	})
 }
